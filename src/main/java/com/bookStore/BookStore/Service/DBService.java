02b8c5fc644a9c1d8b8b00a317d112d96f417433
Joel Santos <joeltere9@gmail.com>
2023-05-10 12:15:22 -0300

    create profile test

--- src/main/java/com/bookStore/BookStore/Service/DBService.java
+++ src/main/java/com/bookStore/BookStore/Service/DBService.java
@@ -21,15 +21,21 @@ public class DBService {
 
     public void instanciaDados() {
 
-        Category category = new Category(null, "Ficção Científica", "Tudo sobre ciencia");
-        Category category2 = new Category(null, "Arte Científica", "Tudo sobre Arte");
+        Category cat1 = new Category(null, "Informática", "Livros de TI");
+        Category cat2 = new Category(null, "Ficcção Científica", "Ficcção científica");
+        Category cat3 = new Category(null, "Biografias", "Livros de Biografias");
 
-        cr.saveAll(Arrays.asList(category, category2));
+        Book l1 = new Book(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+        Book l2 = new Book(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+        Book l3 = new Book(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+        Book l4 = new Book(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+        Book l5 = new Book(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
 
-        Book book = new Book(null, "Arte da guerra", "JKJ", "Aprendo tudo sobre a arte da guerra", category2);
-        Book book2 = new Book(null, "Arte da guerra 2", "JKJ", "Aprendo tudo sobre a arte da guerra", category);
+        cat1.getBooks().addAll(Arrays.asList(l1, l2));
+        cat2.getBooks().addAll(Arrays.asList(l3, l4, l5));
 
-        bs.saveAll(Arrays.asList(book, book2));
+        this.cr.saveAll(Arrays.asList(cat1, cat2, cat3));
+        this.bs.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 
     }
 }
